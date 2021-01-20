b48c8f240a7db552f13af3d2f9c1ecd46f00d0e6
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-01-20 19:01:21 +0500

    Added time

--- app/Algos/ccp.js
+++ app/Algos/ccp.js
@@ -1,4 +1,8 @@
+const performance = require('perf_hooks').performance;
+
 function minCoinChange(amount, coins) {
+  let start_time = performance.now();
+
   const minCoins = new Array(amount + 1).fill(Infinity); 
   minCoins[0] = 0;
   for(let coin of coins) {
@@ -6,7 +10,15 @@ function minCoinChange(amount, coins) {
       if((i - coin) >= 0) minCoins[i] = Math.min(minCoins[i], minCoins[i - coin] + 1);
     }
   }
-  return minCoins[amount] !== Infinity ? minCoins[amount] : -1;
+  let result = minCoins[amount] !== Infinity ? minCoins[amount] : -1;
+  let end_time = performance.now();
+  let timeDiff = end_time - start_time;
+  timeDiff /= 1000;
+  this.time = timeDiff;
+  return {
+    result: result,
+    time: timeDiff
+  }
 }
 
 module.exports = {minCoinChange}
